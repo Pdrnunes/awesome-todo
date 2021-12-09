2b3563e78c8a4484df68fa109111c037e063abe2
Pdrnunes <bttnns@gmail.com>
2021-12-08 21:10:11 -0300

    Creating pages Todo and Settings and referencing them on the navigation bar.

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <h5>This is the Todo page!</h5>
+  </q-page>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
---
+++ src/pages/Settings.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <h5>This is the Settings page!</h5>
+  </q-page>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
