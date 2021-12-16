2f5bd805e34d59355592d5bcc4d8e1e3758fb52b
Pdrnunes <bttnns@gmail.com>
2021-12-16 10:48:18 -0300

    Adding a Vuex store.

---
+++ src/store/store-tasks.js
@@ -0,0 +1,23 @@
+const state = {
+
+}
+
+const mutations = {
+
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+// namespaced - allows the app to have multiple store modules
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    getters,
+    actions
+}
