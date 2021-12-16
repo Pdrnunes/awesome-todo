b6df9d6537c5979d8d4e8bf8e2723681785130cc
Pdrnunes <bttnns@gmail.com>
2021-12-16 20:05:20 -0300

    Adding a vuejs store for the tasks property.

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -60,35 +60,51 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      tasks: [
-        {
+import { mapGetters } from 'vuex'
 
-          id: 1,
-          name: 'Go to Heaven',
-          completed: false,
-          dueDate: '2021/11/04',
-          dueTime: '17:30'
-        },
-        {
-          id: 2,
-          name: 'Go to Hell',
-          completed: false,
-          dueDate: '2021/11/25',
-          dueTime: '13:10'
-        },
-        {
-          id: 3,
-          name: 'Go to Purgatory',
-          completed: false,
-          dueDate: '2021/12/01',
-          dueTime: '08:00'
-        },
-      ]
-    }
-  },
+export default {
+  computed: {
+    // tasks () {
+    /* [tasks/tasks] -> means it will get something from the tasks store module(first tasks)
+    wich in this case will be the tasks getter (second tasks) */
+
+    // return this.$store.getters['tasks/tasks']
+
+    /* in the parenthesis the first argument is for specifying the module needed 
+    and in the second argument is for an array of the gets the component will use*/
+    ...mapGetters('tasks', ['tasks'])
+    /* There is another way to do it by maping it directly to the getter: */
+
+  }
+
+  /* when the getters are ready to use the data property is no longer needed, so i'm commenting it */
+  // data () {
+  //   return {
+  //     tasks: [
+  //       {
+  //         id: 1,
+  //         name: 'Go to Heaven',
+  //         completed: false,
+  //         dueDate: '2021/11/04',
+  //         dueTime: '17:30'
+  //       },
+  //       {
+  //         id: 2,
+  //         name: 'Go to Hell',
+  //         completed: false,
+  //         dueDate: '2021/11/25',
+  //         dueTime: '13:10'
+  //       },
+  //       {
+  //         id: 3,
+  //         name: 'Go to Purgatory',
+  //         completed: false,
+  //         dueDate: '2021/12/01',
+  //         dueTime: '08:00'
+  //       },
+  //     ]
+  //   }
+  // },
 }
 
 </script>
--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,13 +1,39 @@
+// Data goes here:
 const state = {
-
+  tasks: [
+    {
+      id: 1,
+      name: 'Go to Heaven',
+      completed: false,
+      dueDate: '2021/11/04',
+      dueTime: '17:30'
+    },
+    {
+      id: 2,
+      name: 'Go to Hell',
+      completed: false,
+      dueDate: '2021/11/25',
+      dueTime: '13:10'
+    },
+    {
+      id: 3,
+      name: 'Go to Purgatory',
+      completed: false,
+      dueDate: '2021/12/01',
+      dueTime: '08:00'
+    },
+  ]
 }
 
 const mutations = {
 
 }
-
+// getters basically returns something from the state
 const getters = {
 
+  tasks: (state) => {
+    return state.tasks
+  }
 }
 
 const actions = {
@@ -15,9 +41,9 @@ const actions = {
 }
 // namespaced - allows the app to have multiple store modules
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    getters,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+  actions
 }
